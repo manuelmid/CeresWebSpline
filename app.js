aeddceb6047c26b6584b02430d80b52a214bf982
manuelmid <manuelmid99@gmail.com>
2023-01-24 23:27:38 +0100

    working on the information styles

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-import { Application } from '@splinetool/runtime';
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/YnK8zTqEVjok-CP6/scene.splinecode');
+alert("as")
