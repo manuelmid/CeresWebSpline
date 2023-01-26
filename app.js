7857511296a83d0326406dc7c84d1d9777731bac
manuelmid <manuelmid99@gmail.com>
2023-01-26 23:45:46 +0100

    detecting the windows load

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-document.body.addEventListener("click", habla)
+document.addEventListener("click", habla)
 
 function habla(e){
-    alert("asd")
+    //alert("asd")
 }
+
+window.onload = function(){
+    //alert("Carga completada")
+
+}
+
+window.addEventListener("load", function(e){
+    console.log("carga")
+})
+
+let canvas = document.getElementById('canvas3d')
+/* let stateCheck = setInterval( ()=>{
+
+
+    if (canvas === true) {
+          clearInterval(stateCheck);
+          console.log('Funciona')
+    }
+    else{
+        console.log("no working")
+    }
+    console.log(canvas)
+}, 100);
+ */
